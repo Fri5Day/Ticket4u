1d84657bb5a6a141f745d1d71e918decd30eab3b
Fri5Day <64666008+Fri5Day@users.noreply.github.com>
2022-11-06 17:00:09 -0300

    Delete Ticket4u/public directory

--- Ticket4u/public/components/header/header.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementById("header-info");
-
-if (!sessionStorage.getItem("user")) {
-    header.innerHTML += `<span onclick="location.href='/public/login/login.html';" class="navbar-brand mb-0 h1 text-white">Entrar</span>`
-
-} else {
-    header.innerHTML += `<span onclick="location.href='';" class="navbar-brand mb-0 h1 text-white ml-5">${sessionStorage.getItem("user")}</span>`
-
-}
